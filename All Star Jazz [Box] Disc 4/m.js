f21ca111d42dcfa6e29c4b57ac891caa17df04a2
littleflute <13699175164@sina.cn>
2017-05-27 15:54:33 -0500

    Add files via upload

---
+++ All Star Jazz [Box] Disc 4/m.js
@@ -0,0 +1,14 @@
+var f = [];				
+f[0]	= "v0.0.7";			
+f[1]	= "01 You Stepped Out of a Dream.mp3";		
+f[2]	= "02 Stella by Starlight.mp3";		
+f[3]	= "03 Pitchin' Up a Boogie.mp3";		
+f[4]	= "04 Take the Train.mp3";		
+f[5]	= "05 Lush Life.mp3";		
+f[6]	= "06 Makin' Whoopee.mp3";		
+f[7]	= "07 Groovin' High.mp3";		
+f[8]	= "08 Time After Time.mp3";		
+f[9]	= "09 Moonlight in Vermont.mp3";		
+f[10]	= "10 Sugar.mp3";		
+f[11]	= "11 Big Foot.mp3";		
+f[12]	= "12 Humoresque.mp3";		
